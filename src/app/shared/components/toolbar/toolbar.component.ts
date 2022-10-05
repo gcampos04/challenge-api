6a7d14dac1727cba4f6db930b1b6055279ca4e9e
gcampos04 <gcampos02@live.com>
2022-10-05 19:14:29 -0300

    feat: creation of the search method

--- src/app/shared/components/toolbar/toolbar.component.ts
+++ src/app/shared/components/toolbar/toolbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { AppComponent } from 'src/app/app.component';
+import { CharactersService } from 'src/app/characters/services/characters.service';
 
 @Component({
   selector: 'app-toolbar',
@@ -6,7 +8,24 @@ import { Component, Inject, OnInit } from '@angular/core';
   styleUrls: ['./toolbar.component.scss'],
 })
 export class ToolbarComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private sidenav: AppComponent,
+    private _charactersService: CharactersService
+  ) {}
 
   ngOnInit(): void {}
+
+  toggleSideNav() {
+    this.sidenav.toggle();
+  }
+
+  timer: any;
+  search(inputSearch: HTMLInputElement) {
+    if (inputSearch.value == '')
+      this._charactersService.searchCharacters(inputSearch.value);
+    clearTimeout(this.timer);
+    this.timer = setTimeout(() => {
+      this._charactersService.searchCharacters(inputSearch.value);
+    }, 500);
+  }
 }
