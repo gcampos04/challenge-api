c187a3b34bf4a1e8cdeea8336347f479be0ca4d2
gcampos04 <gcampos02@live.com>
2022-10-07 10:17:25 -0300

    feat: adding modal component in shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { MaterialModule } from './material/material.module';
 import { LoadingComponent } from './components/loading/loading.component';
+import { ModalComponent } from './components/modal/modal.component';
 
 @NgModule({
-  declarations: [ToolbarComponent, LoadingComponent],
+  declarations: [ToolbarComponent, LoadingComponent, ModalComponent],
   imports: [CommonModule, MaterialModule],
-  exports: [ToolbarComponent, MaterialModule],
+  exports: [ToolbarComponent, MaterialModule, ModalComponent],
 })
 export class SharedModule {}
