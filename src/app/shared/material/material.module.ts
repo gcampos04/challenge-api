7a0c62aaea110e86d991afe7bc19ced29a13879c
gcampos04 <gcampos02@live.com>
2022-10-07 10:10:36 -0300

    feat: created modal for listing stories and comics

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
@@ -27,6 +29,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatFormFieldModule,
     MatAutocompleteModule,
     MatInputModule,
+    MatDialogModule,
+    MatListModule,
   ],
 })
 export class MaterialModule {}
