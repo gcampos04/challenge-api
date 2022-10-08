297fef15490663b97e9c24c5fec8940c425b1aba
gcampos04 <gcampos02@live.com>
2022-10-08 19:41:51 -0300

    feat: created component error dialog

---
+++ src/app/shared/enum/error-msg.ts
@@ -0,0 +1,5 @@
+export enum ErrorMsgEnum {
+  loadingList = 'Error loading character list.',
+  loadingCharacterSeries = 'Error loading character series list.',
+  loadingCharacterComics = 'Error loading character comics list.',
+}
